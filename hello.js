c181ee630da8c9b80a4aef7fd1979d51a2646913
Sebastian varon <sebastianvaron96@gmail.com>
2023-08-08 00:34:47 +0000

    use return in a function

--- hello.js
+++ hello.js
@@ -5,3 +5,19 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//example usign console.log()
+const sayHelloToConsole = function(name){
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('Jhon');
+
+const returnSayHello  = function(name){
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello("Jhon")
+console.log(greeting);
+
+// the first example is going to print on console the message and the second one save the message into a variable and then you can use the variable to printed out in console or do other stuff
